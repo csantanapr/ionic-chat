137e80d60cfc39caa04f07ed1e6b000413b8960c
Carlos Santana <csantana@us.ibm.com>
2015-09-14 22:40:50 -0400

    Baisic UI working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,10 +4,92 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ChatCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope) {
 
 })
 
-.controller('SettingsCtrl', function($scope) {
-
+.controller('ChatCtrl', function($scope, $ionicScrollDelegate, $ionicActionSheet, $ionicModal, ChatManager, $cordovaCamera, FakeCamera) {
+ 	$scope.handle = localStorage.handle || 'Anonymous';
+	$scope.posts = ChatManager.posts;
+	
+	function addPost(message, img) {
+		ChatManager.add({
+			message: message ? message : null,
+			img: img ? img : null,
+			timestamp: new Date().getTime(),
+			user: $scope.handle
+		});
+		scrollBottom();
+	}
+	
+	function scrollBottom() {
+		$ionicScrollDelegate.$getByHandle('chat').scrollBottom();
+	}
+	//$scope.posts.$watch(scrollBottom);
+	
+	$scope.add = function (message) {
+		addPost(message);
+		$scope.message = null; // pretty things up
+	};
+	
+	$scope.takePicture = function () {
+		$ionicActionSheet.show({
+			buttons: [
+				{
+					text: 'Picture'
+				}, {
+					text: 'Selfie'
+				}, {
+					text: 'Saved Photo'
+				}
+			],
+			titleText: 'Take a...',
+			cancelText: 'Cancel',
+			buttonClicked: function (index) {
+				ionic.Platform.isWebView() ? takeARealPicture(index) : takeAFakePicture();
+				return true;
+			}
+		});
+	
+		function takeARealPicture(cameraIndex) {
+			var options = {
+				quality: 50,
+				sourceType: cameraIndex === 2 ? 2 : 1,
+				cameraDirection: cameraIndex,
+				destinationType: Camera.DestinationType.DATA_URL,
+				encodingType: Camera.EncodingType.JPEG,
+				targetWidth: 500,
+				targetHeight: 600,
+				saveToPhotoAlbum: false
+			};
+			$cordovaCamera.getPicture(options).then(function (imageData) {
+				var photo = "data:image/jpeg;base64," + imageData;
+				addPost(null, photo);
+			}, function (err) {
+				// error
+				console.error(err);
+				takeAFakePicture();
+			});
+		}
+	
+		function takeAFakePicture() {
+			addPost(null, FakeCamera.getPicture());
+		}
+	};
+	
+	$scope.save = function (handle) {
+		localStorage.handle = $scope.handle = handle;
+		$scope.modal.hide();
+	}
+	
+	$ionicModal.fromTemplateUrl('templates/account.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function (modal) {
+		$scope.modal = modal;
+	});
+	
+	$scope.openModal = function () {
+		$scope.modal.show();
+	};
 });
