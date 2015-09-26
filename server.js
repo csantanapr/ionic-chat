c4de4d29a6e1497358c796eb89d8da76a35b2146
Carlos Santana <csantana@us.ibm.com>
2015-09-26 16:12:58 -0400

    remove comments from server

--- server.js
+++ server.js
@@ -91,36 +91,3 @@ io.on('connection', function (socket) {
     }
   });
 });
-
-/* Using WebSockets 
-var express = require('express');
-var cfenv = require('cfenv');
-var app = express();
-var appEnv = cfenv.getAppEnv();
-var server = require('http').createServer();
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ server:server });
-
-app.use(express.static(__dirname + '/www'));
-
-wss.on('connection', function connection(ws) {
-
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    //ws.send(message);
-    //broadcast
-    wss.clients.forEach(function each(client) {
-          client.send(message);
-    });
-  });
-
-  //ws.send('Connection established');
-});
-
-
-server.on('request', app);
-server.listen(appEnv.port, function () { 
-  console.log('Listening on ' + appEnv.url); 
-});
-
-*/
