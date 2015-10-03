dc194962bdc2f344dd8b440733e6945af901a6d3
Carlos Santana <csantana@us.ibm.com>
2015-10-03 17:32:13 -0400

    redirect to https

--- server.js
+++ server.js
@@ -23,6 +23,25 @@ app.use(require('compression')());
 // Routing
 app.use(express.static(__dirname + '/www'));
 
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See 
+// http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+
+// Add a handler to inspect the req.secure flag (see 
+// http://expressjs.com/api#req.secure). This allows us 
+// to know whether the request was via http or https.
+app.use (function (req, res, next) {
+	if (req.secure || req.hostname === 'localhost') {
+		// request was via https, so do no special handling
+		next();
+	} else {
+		// request was via http, so redirect to https
+		res.redirect('https://' + req.headers.host + req.url);
+	}
+});
+
 // Chatroom
 
 // usernames which are currently connected to the chat
