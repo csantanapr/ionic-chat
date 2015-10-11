6c7d3194779c8ee75f33d087ec381733d4c7e98a
Carlos Santana <csantana@us.ibm.com>
2015-10-11 10:09:10 -0400

    initial cloudant integration

--- server.js
+++ server.js
@@ -9,10 +9,66 @@ var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
 //var port = process.env.PORT || 3000;
 var port = appEnv.port;
+var dbService = appEnv.getService('cloudant-chat');
+var db;
+var cloudant;
+var dbCredentials = {
+  dbName: 'test_db1'
+};
+function initDBConnection() {
+  if (dbService) {
+    dbCredentials.host = dbService.credentials.host;
+    dbCredentials.port = dbService.credentials.port;
+    dbCredentials.user = dbService.credentials.username;
+    dbCredentials.password = dbService.credentials.password;
+    dbCredentials.url = dbService.credentials.url;
+
+    cloudant = require('cloudant')(dbCredentials.url);
+			
+    // check if DB exists if not create
+    cloudant.db.get(dbCredentials.dbName, function (err, body) {
+      if (err) {
+        cloudant.db.create(dbCredentials.dbName, function (err, res) {
+          if (err) { console.log('could not create db ', err); }
+          db = cloudant.use(dbCredentials.dbName);
+        });
+      } else {
+        db = cloudant.use(dbCredentials.dbName);
+      }
+
+    });
+
+  } else {
+    console.warn('VCAP_SERVICES environment variable not set - data will be unavailable to the UI');
+    // For running this app locally you can get your Cloudant credentials 
+    // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment 
+    // Variables section for an app in the Bluemix console dashboard).
+    // Alternately you could point to a local database here instead of a 
+    // Bluemix service.
+    //dbCredentials.host = "REPLACE ME";
+    //dbCredentials.port = REPLACE ME;
+    //dbCredentials.user = "REPLACE ME";
+    //dbCredentials.password = "REPLACE ME";
+    //dbCredentials.url = "REPLACE ME";
+  }
+}
+function destroyDB() {
+  cloudant.db.destroy(dbCredentials.dbName, function (err) {
+    if (!err) { console.log(dbCredentials.dbName + ' db detroyed'); }
+  });
+}
+
+initDBConnection();
+
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+
+app.get('/destroydb',function(req,res){
+  destroyDB();
+});
+
 
 // Enable reverse proxy support in Express. This causes the
 // the "X-Forwarded-Proto" header field to be trusted so its
@@ -23,14 +79,14 @@ app.enable('trust proxy');
 // Add a handler to inspect the req.secure flag (see 
 // http://expressjs.com/api#req.secure). This allows us 
 // to know whether the request was via http or https.
-app.use (function (req, res, next) {
-	if (isSecure(req)) {
-		// request was via https, so do no special handling
-		next();
-	} else {
-		// request was via http, so redirect to https
-		res.redirect('https://' + req.hostname + req.originalUrl);
-	}
+app.use(function (req, res, next) {
+  if (isSecure(req)) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.hostname + req.originalUrl);
+  }
 });
 
 //Compress
@@ -44,18 +100,18 @@ app.use(express.static(__dirname + '/www'));
 
 
 function isSecure(req) {
-    // Check the trivial case first.
-    if (req.secure || /mybluemix.net/i.test(req.hostname) === false) {
-        return true;
-    }
-    // Check if we are behind Application Request Routing (ARR).
-    // This is typical for Azure.
-    if (req.headers['x-arr-log-id']) {
-        return typeof req.headers['x-arr-ssl'] === 'string';
-    }
-    // Check for forwarded protocol header.
-    // This is typical for AWS.
-    return req.headers['x-forwarded-proto'] === 'https';
+  // Check the trivial case first.
+  if (req.secure || /mybluemix.net/i.test(req.hostname) === false) {
+    return true;
+  }
+  // Check if we are behind Application Request Routing (ARR).
+  // This is typical for Azure.
+  if (req.headers['x-arr-log-id']) {
+    return typeof req.headers['x-arr-ssl'] === 'string';
+  }
+  // Check for forwarded protocol header.
+  // This is typical for AWS.
+  return req.headers['x-forwarded-proto'] === 'https';
 }
 
 // Chatroom
@@ -74,8 +130,16 @@ io.on('connection', function (socket) {
       //username: socket.username,
       message: data
     });
-    
-    console.log(data.user + (data.img ? ' sent an img' : ' say: '+data.message));
+
+    console.log(data.user + (data.img ? ' sent an img' : ' say: ' + data.message));
+    //save image to the cloud
+    if (data.img) {
+      db.insert(data, function (err, body) {
+        if (!err)
+          console.log(body);
+      });
+    }
+
   });
 
   // when the client emits 'add user', this listens and executes
